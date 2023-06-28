22498c27e489ee91e25386bf714f5249918e3855
Fellipe97 <lfalmeida97@hotmail.com>
2023-06-28 00:38:01 -0300

    feat: :sparkles: layout da tela de contatos concluido

--- src/screens/Contacts.tsx
+++ src/screens/Contacts.tsx
@@ -1,8 +1,10 @@
-import { Text, VStack } from "native-base";
+import { HStack, ScrollView, Text, VStack } from "native-base";
 
 import { useNavigation } from '@react-navigation/native'
 
 import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import Input from "@components/Input";
 
 export function Contacts() {
     const navigation = useNavigation();
@@ -12,9 +14,59 @@ export function Contacts() {
     }
 
 
-    return(
-        <VStack mt={10} flex={1} px={12} pb={16} alignItems='center' justifyContent='center'>
-            <Text>Contacts</Text>
-        </VStack>
+    return (
+        <>
+            <Header
+                title="Contatos"
+                buttonBack={false}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <VStack mt={6} flex={1} px={12} pb={7}>
+                    <VStack flex={1}>
+                        <HStack /* bg={'red.100'} */ justifyContent={'space-between'}>
+                            <VStack /* bg={'green.100'} */ alignItems={'center'}>
+                                <Text fontFamily={'heading'} fontSize={'xl'}>Telefone</Text>
+                                <Text mt={3} color={'purple.600'} fontSize={'md'} fontFamily={'heading'}>(21) 00000-0000</Text>
+                                <Text color={'purple.600'} fontSize={'md'} fontFamily={'heading'}>(21) 00000-0000</Text>
+                            </VStack>
+                            <VStack /* bg={'blue.100'} */ alignItems={'center'}>
+                                <Text fontFamily={'heading'} fontSize={'xl'}>Whatsapp</Text>
+                                <Text mt={3} color={'purple.600'} fontSize={'md'} fontFamily={'heading'}>(21) 00000-0000</Text>
+                            </VStack>
+                        </HStack>
+
+                        <VStack mt={6} alignItems={'center'}>
+                            <Text fontFamily={'heading'} fontSize={'xl'}>ENVIE UMA MENSAGEM</Text>
+
+                            <Input
+                                mt={3}
+                                placeholder="Nome"
+                            />
+                            <Input
+                                placeholder="Email"
+                            />
+                            <Input
+                                placeholder="Telefone/Celular"
+                            />
+                            <Input
+                                placeholder="Em que podemos ajudar?"
+                                multiline={true}
+                                numberOfLines={5}
+                                h={120}
+                                textAlignVertical="top" 
+                            />
+                        </VStack>
+
+                    </VStack>
+
+                    <VStack>
+                        <Button
+                            title="Enviar"
+                            onPress={() => console.log('Enviando o email')}
+                        />
+                    </VStack>
+                </VStack>
+            </ScrollView>
+        </>
     );
 }
