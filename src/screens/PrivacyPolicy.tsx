8ff9cc2f0c5a4e7cf0ecd0eed1840dd5744f1de0
Fellipe97 <lfalmeida97@hotmail.com>
2023-07-05 00:05:33 -0300

    feat: :sparkles: layout do termo de privacidade

---
+++ src/screens/PrivacyPolicy.tsx
@@ -0,0 +1,91 @@
+import { ScrollView, Text, VStack } from "native-base";
+
+import { Header } from "@components/Header";
+
+
+export function PrivacyPolicy() {
+
+    return (
+        <>
+            <Header
+                title="Política de Privacidade"
+                buttonBack={true}
+            />
+            <VStack  px={2}>
+                <VStack mt={10}  pb={8} >
+                    <ScrollView px={5}>
+                        <Text
+                            textAlign={'justify'}
+                            fontFamily={'body'}
+                        >
+                            SEÇÃO 1 - INFORMAÇÕES GERAIS{'\n'}
+                            A presente Política de Privacidade contém informações sobre coleta, uso, armazenamento, tratamento e proteção dos dados pessoais dos usuários e visitantes do aplicativo SmartCurly, com a finalidade de demonstrar absoluta transparência quanto ao assunto e esclarecer a todos interessados sobre os tipos de dados que são coletados, os motivos da coleta e a forma como os usuários podem gerenciar ou excluir as suas informações pessoais.{'\n'}
+                            Esta Política de Privacidade aplica-se a todos os usuários e visitantes do aplicativo SmartCurly e integra os Termos e Condições Gerais de Uso do aplicativo SmartCurly, devidamente inscrita no CPF sob o nº 15056857700, doravante nominada SmartCurly.{'\n'}
+                            O presente documento foi elaborado em conformidade com a Lei Geral de Proteção de Dados Pessoais (Lei 13.709/18), o Marco Civil da Internet (Lei 12.965/14) (e o Regulamento da UE n. 2016/6790). Ainda, o documento poderá ser atualizado em decorrência de eventual atualização normativa, razão pela qual se convida o usuário a consultar periodicamente esta seção.{'\n'}
+                            {'\n'}SEÇÃO 2 - COMO RECOLHEMOS OS DADOS PESSOAIS DO USUÁRIO E DO VISITANTE?{'\n'}
+                            Os dados pessoais do usuário e visitante são recolhidos pela plataforma da seguinte forma:{'\n'}
+                            (Realizar pergunta de múltipla escolha com opção outras){'\n'}
+                            ●	Quando o usuário cria uma conta/perfil na plataforma SmartCurly: esses dados são os dados de identificação básicos, como: e-mail, nome completo, telefone e data de nascimento). A partir deles, podemos identificar o usuário e o visitante, além de garantir uma maior segurança e bem-estar às suas necessidade. Ficam cientes os usuários e visitantes de que seu perfil na plataforma estará acessível a todos demais usuários e visitantes da plataforma SmartCurly.{'\n'}
+                            ●	Quando um usuário e visitante acessa o aplicativo SmartCurly: as informações sobre interação e acesso são coletadas pela empresa para garantir uma melhor experiência ao usuário e visitante. Estes dados podem tratar sobre as palavras-chaves utilizadas em uma busca, o compartilhamento de um documento específico, comentários, visualizações de páginas, perfis, a URL de onde o usuário e visitante provêm, o navegador que utilizam e seus IPs de acesso, dentre outras que poderão ser armazenadas e retidas.{'\n'}
+                            ●	Por intermédio de terceiro: a plataforma SmartCurly recebe dados de terceiros, como Facebook e Google, quando um usuário faz login com o seu perfil de um desses sites. A utilização desses dados é autorizada previamente pelos usuários junto ao terceiro em questão.{'\n'}
+                            ●	outras{'\n'}
+                            {'\n'}SEÇÃO 3 - QUAIS DADOS PESSOAIS RECOLHEMOS SOBRE O USUÁRIO E VISITANTE?{'\n'}
+                            Os dados pessoais do usuário e visitante recolhidos são os seguintes:{'\n'}
+                            ●	Dados para a criação da conta/perfil na plataforma SmartCurly: e-mail, nome completo, telefone e data de nascimento.{'\n'}
+                            ●	Dados para otimização da navegação: acesso a páginas, palavras-chave utilizadas na busca, recomendações, comentários, interação com outros perfis e usuários, perfis seguidos, endereço de IP.{'\n'}
+                            ●	Dados para concretizar transações: dados referentes ao pagamento e transações, tais como, número do cartão de crédito e outras informações sobre o cartão, além dos pagamentos efetuados.{'\n'}
+                            ●	Newsletter: o e-mail cadastrado pelo visitante que optar por se inscrever na Newsletter será coletado e armazenado até que o usuário solicite o descadastro.{'\n'}
+                            ●	Dados sensíveis: a plataforma poderá coletar os seguintes dados sensíveis do usuário - orientação de gênero e características capilar.{'\n'}
+                            ●	Dados relacionados a contratos: diante da formalização do contrato de compra e venda ou de prestação de serviços entre a plataforma e o usuário e visitante poderão ser coletados e armazenados dados relativos a execução contratual, inclusive as comunicações realizada entre a empresa e o usuário.{'\n'}
+                            ●	outras{'\n'}
+                            {'\n'}SEÇÃO 4 - PARA QUE FINALIDADES UTILIZAMOS OS DADOS PESSOAIS DO USUÁRIO E VISITANTE?{'\n'}
+                            Os dados pessoais do usuário e do visitante coletados e armazenados pela plataforma SmartCurly tem por finalidade:{'\n'}
+                            ●	Bem-estar do usuário e visitante: aprimorar o produto e/ou serviço oferecido, facilitar, agilizar e cumprir os compromissos estabelecidos entre o usuário e a empresa, melhorar a experiência dos usuários e fornecer funcionalidades específicas a depender das características básicas do usuário.{'\n'}
+                            ●	Melhorias da plataforma: compreender como o usuário utiliza os serviços da plataforma, para ajudar no desenvolvimento de negócios e técnicas.{'\n'}
+                            ●	Anúncios: apresentar anúncios personalizados para o usuário com base nos dados fornecidos.{'\n'}
+                            ●	Comercial: os dados são usados para personalizar o conteúdo oferecido e gerar subsídio à plataforma para a melhora da qualidade no funcionamento dos serviços.{'\n'}
+                            ●	Previsão do perfil do usuário: tratamento automatizados de dados pessoais para avaliar o uso na plataforma.{'\n'}
+                            ●	Dados de cadastro: para permitir o acesso do usuário a determinados conteúdos da plataforma, exclusivo para usuários cadastrados{'\n'}
+                            ●	Dados de contrato: conferir às partes segurança jurídica e facilitar a conclusão do negócio.{'\n'}
+                            ●	Outras{'\n'}
+                            O tratamento de dados pessoais para finalidades não previstas nesta Política de Privacidade somente ocorrerá mediante comunicação prévia ao usuário, de modo que os direitos e obrigações aqui previstos permanecem aplicáveis.{'\n'}
+                            {'\n'}SEÇÃO 5 - POR QUANTO TEMPO OS DADOS PESSOAIS FICAM ARMAZENADOS?{'\n'}
+                            Os dados pessoais do usuário e visitante são armazenados pela plataforma durante o período necessário para a prestação do serviço ou o cumprimento das finalidades previstas no presente documento, conforme o disposto no inciso I do artigo 15 da Lei 13.709/18.{'\n'}
+                            Os dados podem ser removidos ou anonimizados a pedido do usuário, excetuando os casos em que a lei oferecer outro tratamento.{'\n'}
+                            Ainda, os dados pessoais dos usuários apenas podem ser conservados após o término de seu tratamento nas seguintes hipóteses previstas no artigo 16 da referida lei:{'\n'}
+                            I - cumprimento de obrigação legal ou regulatória pelo controlador;{'\n'}
+                            II - estudo por órgão de pesquisa, garantida, sempre que possível, a anonimização dos dados pessoais;{'\n'}
+                            III - transferência a terceiro, desde que respeitados os requisitos de tratamento de dados dispostos nesta Lei;{'\n'}
+                            IV - uso exclusivo do controlador, vedado seu acesso por terceiro, e desde que anonimizados os dados.{'\n'}
+                            {'\n'}SEÇÃO 6 - SEGURANÇA DOS DADOS PESSOAIS ARMAZENADOS{'\n'}
+                            A plataforma se compromete a aplicar as medidas técnicas e organizativas aptas a proteger os dados pessoais de acessos não autorizados e de situações de destruição, perda, alteração, comunicação ou difusão de tais dados.{'\n'}
+                            Os dados relativas a cartões de crédito são criptografados usando a tecnologia "secure socket layer" (SSL) que garante a transmissão de dados de forma segura e confidencial, de modo que a transmissão dos dados entre o servidor e o usuário ocorre de maneira cifrada e encriptada.{'\n'}
+                            A plataforma não se exime de responsabilidade por culpa exclusiva de terceiro, como em caso de ataque de hackers ou crackers, ou culpa exclusiva do usuário, como no caso em que ele mesmo transfere seus dados a terceiros. O site se compromete a comunicar o usuário em caso de alguma violação de segurança dos seus dados pessoais.{'\n'}
+                            Os dados pessoais armazenados são tratados com confidencialidade, dentro dos limites legais. No entanto, podemos divulgar suas informações pessoais caso sejamos obrigados pela lei para fazê-lo ou se você violar nossos Termos de Serviço.{'\n'}
+                            {'\n'}SEÇÃO 7 - COMPARTILHAMENTO DOS DADOS{'\n'}
+                            O compartilhamento de dados do usuário ocorre apenas com os dados referentes a publicações realizadas pelo próprio usuário, tais ações são compartilhadas publicamente com os outros usuários.{'\n'}
+                            Os dados do perfil do usuário são compartilhados publicamente em sistemas de busca e dentro da plataforma, sendo permitido ao usuário modificar tal configuração para que seu perfil não apareça nos resultados de busca de tais ferramentas.{'\n'}
+                            {'\n'}SEÇÃO 8 – COOKIES OU DADOS DE NAVEGAÇÃO{'\n'}
+                            Os cookies referem-se a arquivos de texto enviados pela plataforma ao computador do usuário e visitante e que nele ficam armazenados, com informações relacionadas à navegação no site. Tais informações são relacionadas aos dados de acesso como local e horário de acesso e são armazenadas pelo navegador do usuário e visitante para que o servidor da plataforma possa lê-las posteriormente a fim de personalizar os serviços da plataforma.{'\n'}
+                            O usuário e o visitante da plataforma SmartCurly manifesta conhecer e aceitar que pode ser utilizado um sistema de coleta de dados de navegação mediante à utilização de cookies.{'\n'}
+                            O cookie persistente permanece no disco rígido do usuário e visitante depois que o navegador é fechado e será usado pelo navegador em visitas subsequentes ao site. Os cookies persistentes podem ser removidos seguindo as instruções do seu navegador. Já o cookie de sessão é temporário e desaparece depois que o navegador é fechado. É possível redefinir seu navegador da web para recusar todos os cookies, porém alguns recursos da plataforma podem não funcionar corretamente se a capacidade de aceitar cookies estiver desabilitada.{'\n'}
+                            {'\n'}SEÇÃO 9 - CONSENTIMENTO{'\n'}
+                            Ao utilizar os serviços e fornecer as informações pessoais na plataforma, o usuário está consentindo com a presente Política de Privacidade.{'\n'}
+                            O usuário, ao cadastrar-se, manifesta conhecer e pode exercitar seus direitos de cancelar seu cadastro, acessar e atualizar seus dados pessoais e garante a veracidade das informações por ele disponibilizadas.{'\n'}
+                            O usuário tem direito de retirar o seu consentimento a qualquer tempo, para tanto deve entrar em contato através do e-mail equipesmartcurly@gmail.com{'\n'}
+                            {'\n'}SEÇÃO 10 - ALTERAÇÕES PARA ESSA POLÍTICA DE PRIVACIDADE{'\n'}
+                            Reservamos o direito de modificar essa Política de Privacidade a qualquer momento, então, é recomendável que o usuário e visitante revise-a com frequência.{'\n'}
+                            As alterações e esclarecimentos vão surtir efeito imediatamente após sua publicação na plataforma. Quando realizadas alterações os usuários serão notificados. Ao utilizar o serviço ou fornecer informações pessoais após eventuais modificações, o usuário e visitante demonstra sua concordância com as novas normas.{'\n'}
+                            Diante da fusão ou venda da plataforma à outra empresa os dados dos usuários podem ser transferidas para os novos proprietários para que a permanência dos serviços oferecidos.{'\n'}
+                            {'\n'}SEÇÃO 11 – JURISDIÇÃO PARA RESOLUÇÃO DE CONFLITOS{'\n'}
+                            Para a solução de controvérsias decorrentes do presente instrumento será aplicado integralmente o Direito brasileiro.{'\n'}
+                            Os eventuais litígios deverão ser apresentados no foro da comarca em que se encontra a sede da empresa.{'\n'}
+                        </Text>
+
+                    </ScrollView>
+
+                </VStack>
+            </VStack>
+        </>
+    );
+}
