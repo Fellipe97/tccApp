0c69bd5fe6311b442a03d69257fd722f91094337
Fellipe97 <lfalmeida97@hotmail.com>
2023-07-05 00:05:59 -0300

    feat: :sparkles: layout do termo de uso

---
+++ src/screens/TermOfUse.tsx
@@ -0,0 +1,106 @@
+import { ScrollView, Text, VStack } from "native-base";
+
+import { Header } from "@components/Header";
+
+
+export function TermOfUse() {
+
+    return (
+        <>
+            <Header
+                title="Termo de Uso"
+                buttonBack={true}
+            />
+            <VStack px={2}>
+                <VStack mt={10} pb={8} >
+                    <ScrollView px={5}>
+                        <Text
+                            textAlign={'justify'}
+                            fontFamily={'body'}
+                        >
+                            Os serviços do SmartCurly são fornecidos pela pessoa física com o seguinte nome: Débora Vitória da Silva de Oliveira, com nome fantasia SmartCurly, inscrito no CPF sob o nº 15056857700, titular da propriedade intelectual sobre software, website, aplicativos, conteúdos e demais ativos relacionados à plataforma SmartCurly.
+                            {'\n'}{'\n'}1. Do objeto{'\n'}
+                            A plataforma visa licenciar o uso de seu software, website, aplicativos e demais ativos de propriedade intelectual, fornecendo ferramentas para auxiliar e dinamizar o dia a dia dos seus usuários.{'\n'}
+                            A plataforma caracteriza-se pela prestação do seguinte serviço: auxiliar o usuário na escolha do produto ideal para seu tipo de cabelo, selecionando os produtos de acordo com a preferência e necessidades de cada usuário.{'\n'}
+                            A plataforma realiza a venda à distância por meio eletrônico dos seguintes produtos ou serviços: funcionalidades de alertas sobre substâncias que o usuário deseja evitar que estão presentes em alguns produtos.{'\n'}{'\n'}
+                            2. Da aceitação{'\n'}
+                            O presente Termo estabelece obrigações contratadas de livre e espontânea vontade, por tempo indeterminado, entre a plataforma e as pessoas físicas ou jurídicas, usuárias do aplicativo.{'\n'}
+                            Ao utilizar a plataforma o usuário aceita integralmente as presentes normas e compromete-se a observá-las, sob o risco de aplicação das penalidade cabíveis.{'\n'}
+                            A aceitação do presente instrumento é imprescindível para o acesso e para a utilização de quaisquer serviços fornecidos pela empresa. Caso não concorde com as disposições deste instrumento, o usuário não deve utilizá-los.{'\n'}{'\n'}
+                            3. Do acesso dos usuários{'\n'}
+                            Serão utilizadas todas as soluções técnicas à disposição do responsável pela plataforma para permitir o acesso ao serviço 24 (vinte e quatro) horas por dia, 7 (sete) dias por semana. No entanto, a navegação na plataforma ou em alguma de suas páginas poderá ser interrompida, limitada ou suspensa para atualizações, modificações ou qualquer ação necessária ao seu bom funcionamento.{'\n'}{'\n'}
+                            4. Do cadastro{'\n'}
+                            O acesso às funcionalidades da plataforma exigirá a realização de um cadastro prévio e, a depender dos serviços ou produtos escolhidos, o pagamento de determinado valor.{'\n'}
+                            Ao se cadastrar o usuário deverá informar dados completos, recentes e válidos, sendo de sua exclusiva responsabilidade manter referidos dados atualizados, bem como o usuário se compromete com a veracidade dos dados fornecidos.{'\n'}
+                            O usuário se compromete a não informar seus dados cadastrais e/ou de acesso à plataforma a terceiros, responsabilizando-se integralmente pelo uso que deles seja feito.{'\n'}
+                            Menores de 18 anos e aqueles que não possuírem plena capacidade civil deverão obter previamente o consentimento expresso de seus responsáveis legais para utilização da plataforma e dos serviços ou produtos, sendo de responsabilidade exclusiva dos mesmos o eventual acesso por menores de idade e por aqueles que não possuem plena capacidade civil sem a prévia autorização.{'\n'}
+                            Mediante a realização do cadastro o usuário declara e garante expressamente ser plenamente capaz, podendo exercer e usufruir livremente dos serviços e produtos.{'\n'}
+                            O usuário deverá fornecer um endereço de e-mail válido, através do qual o site realizará todas comunicações necessárias.{'\n'}
+                            Após a confirmação do cadastro, o usuário possuirá um login e uma senha pessoal, a qual assegura ao usuário o acesso individual à mesma. Desta forma, compete ao usuário exclusivamente a manutenção de referida senha de maneira confidencial e segura, evitando o acesso indevido às informações pessoais.{'\n'}
+                            Toda e qualquer atividade realizada com o uso da senha será de responsabilidade do usuário, que deverá informar prontamente a plataforma em caso de uso indevido da respectiva senha.{'\n'}
+                            Não será permitido ceder, vender, alugar ou transferir, de qualquer forma, a conta, que é pessoal e intransferível.{'\n'}
+                            Caberá ao usuário assegurar que o seu equipamento seja compatível com as características técnicas que viabilize a utilização da plataforma e dos serviços ou produtos.{'\n'}    
+                            O usuário poderá, a qualquer tempo, requerer o cancelamento de seu cadastro junto ao aplicativo SmartCurly. O seu descadastramento será realizado o mais rapidamente possível, desde que não sejam verificados débitos em aberto.{'\n'}
+                            O usuário, ao aceitar os Termos e Política de Privacidade, autoriza expressamente a plataforma a coletar, usar, armazenar, tratar, ceder ou utilizar as informações derivadas do uso dos serviços, do site e quaisquer plataformas, incluindo todas as informações preenchidas pelo usuário no momento em que realizar ou atualizar seu cadastro, além de outras expressamente descritas na Política de Privacidade que deverá ser autorizada pelo usuário.{'\n'}{'\n'}
+                            5. Dos serviços ou produtos{'\n'}
+                            A plataforma poderá disponibilizar para o usuário um conjunto específico de funcionalidades e ferramentas para otimizar o uso dos serviços e produtos.{'\n'}
+                            Na plataforma os serviços ou produtos oferecidos estão descritos e apresentados com o maior grau de exatidão, contendo informações sobre suas características, qualidades, quantidades, composição, preço, garantia, prazos de validade e origem, entre outros dados, bem como sobre os riscos que apresentam à saúde e segurança do usuário.{'\n'}
+                            Antes de finalizar a compra sobre determinado produto ou serviço, o usuário deverá se informar sobre as suas especificações e sobre a sua destinação.{'\n'}
+                            A entrega de serviços ou produtos adquiridos na plataforma será informada no momento da finalização da compra.{'\n'}{'\n'}
+                            6. Dos preços{'\n'}
+                            A plataforma se reserva no direito de reajustar unilateralmente, a qualquer tempo, os valores dos serviços ou produtos sem consulta ou anuência prévia do usuário.{'\n'}
+                            Os valores aplicadas são aqueles que estão em vigor no momento do pedido.{'\n'}
+                            Os preços são indicados em reais e não incluem as taxas de entrega, as quais são especificadas à parte e são informadas ao usuário antes da finalização do pedido.{'\n'}
+                            Na contratação de determinado serviço ou produto, a plataforma poderá solicitar as informações financeiras do usuário, como CPF, endereço de cobrança e dados de cartões. Ao inserir referidos dados o usuário concorda que sejam cobrados, de acordo com a forma de pagamento que venha a ser escolhida, os preços então vigentes e informados quando da contratação. Referidos dados financeiros poderão ser armazenadas para facilitar acessos e contratações futuras.{'\n'}
+                            A contratação dos serviços será renovada automaticamente pela plataforma, independentemente de comunicação ao usuário, mediante cobrança periódica da mesma forma de pagamento indicada pelo usuário quando da contratação do serviço.{'\n'}{'\n'}
+                            7. Do cancelamento{'\n'}
+                            O usuário poderá cancelar a contratação dos serviços de acordo com os termos que forem definidos no momento de sua contratação. Ainda, o usuário também poderá cancelar os serviços em até 7 (sete) dias após a contratação, mediante contato com a equipe SmartCurly, de acordo com o Código de Defesa do Consumidor (Lei no. 8.078/90).{'\n'}
+                            O serviço poderá ser cancelado por:{'\n'}
+                            a&#41; parte do usuário: nessas condições os serviços somente cessarão quando concluído o ciclo vigente ao tempo do cancelamento;{'\n'}
+                            b&#41; violação dos Termos de Uso: os serviços serão cessados imediatamente.{'\n'}{'\n'}
+                            8. Do suporte{'\n'}
+                            Em caso de qualquer dúvida, sugestão ou problema com a utilização da plataforma, o usuário poderá entrar em contato com o suporte, através do email equipesmartcurly@gmail.com.{'\n'}
+                            Estes serviços de atendimento ao usuário estarão disponíveis nos seguintes dias e horários: segunda-feira a sexta-feira de 08:00 ás 17:00 horas.{'\n'}{'\n'}
+                            9. Das responsabilidades{'\n'}
+                            É de responsabilidade do usuário:{'\n'}
+                            a&#41; defeitos ou vícios técnicos originados no próprio sistema do usuário;{'\n'}
+                            b&#41; a correta utilização da plataforma, dos serviços ou produtos oferecidos, prezando pela boa convivência, pelo respeito e cordialidade entre os usuários;{'\n'}
+                            c&#41; pelo cumprimento e respeito ao conjunto de regras disposto nesse Termo de Condições Geral de Uso, na respectiva Política de Privacidade e na legislação nacional e internacional;{'\n'}
+                            d&#41; pela proteção aos dados de acesso à sua conta/perfil (login e senha).{'\n'}
+                            É de responsabilidade da plataforma SmartCurly:{'\n'}
+                            a&#41; indicar as características do serviço ou produto;{'\n'}
+                            b&#41; os defeitos e vícios encontrados no serviço ou produto oferecido desde que lhe tenha dado causa;{'\n'}
+                            c&#41; as informações que foram por ele divulgadas, sendo que os comentários ou informações divulgadas por usuários são de inteira responsabilidade dos próprios usuários;{'\n'}
+                            d&#41; os conteúdos ou atividades ilícitas praticadas através da sua plataforma.{'\n'}
+                            A plataforma não se responsabiliza por links externos contidos em seu sistema que possam redirecionar o usuário à ambiente externo a sua rede.{'\n'}
+                            Não poderão ser incluídos links externos ou páginas que sirvam para fins comerciais ou publicitários ou quaisquer informações ilícitas, violentas, polêmicas, pornográficas, xenofóbicas, discriminatórias ou ofensivas.{'\n'}{'\n'}
+                            10. Dos direitos autorais{'\n'}
+                            O presente Termo de Uso concede aos usuários uma licença não exclusiva, não transferível e não sublicenciável, para acessar e fazer uso da plataforma e dos serviços e produtos por ela disponibilizados.{'\n'}
+                            A estrutura do site ou aplicativo, as marcas, logotipos, nomes comerciais, layouts, gráficos e design de interface, imagens, ilustrações, fotografias, apresentações, vídeos, conteúdos escritos e de som e áudio, programas de computador, banco de dados, arquivos de transmissão e quaisquer outras informações e direitos de propriedade intelectual da razão social SmartCurly, observados os termos da Lei da Propriedade Industrial (Lei nº 9.279/96), Lei de Direitos Autorais (Lei nº 9.610/98) e Lei do Software (Lei nº 9.609/98), estão devidamente reservados.{'\n'}
+                            Este Termos de Uso não cede ou transfere ao usuário qualquer direito, de modo que o acesso não gera qualquer direito de propriedade intelectual ao usuário, exceto pela licença limitada ora concedida.{'\n'}
+                            O uso da plataforma pelo usuário é pessoal, individual e intransferível, sendo vedado qualquer uso não autorizado, comercial ou não-comercial. Tais usos consistirão em violação dos direitos de propriedade intelectual da razão social SmartCurly, puníveis nos termos da legislação aplicável.{'\n'}{'\n'}
+                            11. Das sanções{'\n'}
+                            Sem prejuízo das demais medidas legais cabíveis, a razão social SmartCurly poderá, a qualquer momento, advertir, suspender ou cancelar a conta do usuário:{'\n'}
+                            a&#41; que violar qualquer dispositivo do presente Termo;{'\n'}
+                            b&#41; que descumprir os seus deveres de usuário;{'\n'}
+                            c&#41; que tiver qualquer comportamento fraudulento, doloso ou que ofenda a terceiros.{'\n'}{'\n'}
+                            12. Da rescisão{'\n'}
+                            A não observância das obrigações pactuadas neste Termo de Uso ou da legislação aplicável poderá, sem prévio aviso, ensejar a imediata rescisão unilateral por parte da razão social SmartCurly e o bloqueio de todos os serviços prestados ao usuário.{'\n'}{'\n'}
+                            13. Das alterações{'\n'}
+                            Os itens descritos no presente instrumento poderão sofrer alterações, unilateralmente e a qualquer tempo, por parte da equipe da SmartCurly, para adequar ou modificar os serviços, bem como para atender novas exigências legais. As alterações serão veiculadas pelo aplicativo SmartCurly e o usuário poderá optar por aceitar o novo conteúdo ou por cancelar o uso dos serviços, caso seja assinante de algum serviço.{'\n'}
+                            Os serviços oferecidos podem, a qualquer tempo e unilateralmente, e sem qualquer aviso prévio, ser deixados de fornecer, alterados em suas características, bem como restringido para o uso ou acesso.{'\n'}{'\n'}
+                            14. Da política de privacidade{'\n'}
+                            Além do presente Termo, o usuário deverá consentir com as disposições contidas na respectiva Política de Privacidade a ser apresentada a todos os interessados dentro da interface da plataforma.{'\n'}{'\n'}
+                            15. Do foro{'\n'}
+                            Para a solução de controvérsias decorrentes do presente instrumento será aplicado integralmente o Direito brasileiro.{'\n'}
+                            Os eventuais litígios deverão ser apresentados no foro da comarca em que se encontra a sede da empresa.{'\n'}
+
+                        </Text>
+
+                    </ScrollView>
+
+                </VStack>
+            </VStack>
+        </>
+    );
+}
