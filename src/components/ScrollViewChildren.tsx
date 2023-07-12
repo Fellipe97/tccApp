2822dc41b14b9c447b328b06c90393e067fe4bbf
Fellipe97 <lfalmeida97@hotmail.com>
2023-07-12 00:43:32 -0300

    feat: :sparkles: butao do aluno

---
+++ src/components/ScrollViewChildren.tsx
@@ -0,0 +1,68 @@
+import { Spinner, Center, Image, VStack, Text, ScrollView, Pressable, HStack, Avatar } from 'native-base'
+
+import LogoSvg from '@assets/logo.svg'
+
+
+type PropsChildren = {
+    name: string;
+    birth: string;
+    registration: string;
+    bloodType: string;
+    grade: string;
+    photograph: string;
+}
+type Props = {
+    isActive ?: boolean;
+    item: PropsChildren;
+    onPress: () => void
+}
+
+export function ScrollViewChildren({ isActive = false, item, onPress}: Props) {
+    return (
+        <Pressable
+            width={190}
+            height={100}
+            bg={'white'}
+            rounded={10}
+            shadow={7}
+            ml={2}
+            mr={2}
+            //key={index}
+            borderColor={'purple.600'}
+            borderWidth={ isActive ? 3 : 0}            
+            _pressed={{
+                opacity: .7
+            }}
+            onPress={onPress}
+        >
+            <HStack h={'full'}>
+
+                <VStack w={'60%'} p={3} justifyContent={'center'}>
+                    <Text
+                        fontFamily={'heading'}
+                        fontSize={'md'}
+                    >{item.name.split(" ")[0] + '\n' + item.name.split(" ")[1]}</Text>
+                    <Text
+                        fontFamily={'body'}
+                        color={'gray.500'}
+                    >{item.registration}</Text>
+
+                </VStack>
+
+                <VStack w={'40%'} /* bg={'green.100'} w={'30%'} */
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                >
+
+                    <Avatar bg="gray.500" size={'lg'} shadow={7} source={{
+                        uri: item.photograph
+                    }}>
+                        ...
+                    </Avatar>
+
+                </VStack>
+
+            </HStack>
+        </Pressable>
+    );
+}
