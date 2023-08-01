b5cee427b1008cf647fa4a3e1a39cdb5c41f68bd
Fellipe97 <lfalmeida97@hotmail.com>
2023-08-01 08:05:58 -0300

    feat: :sparkles: tipagem da criança

---
+++ src/types/children.tsx
@@ -0,0 +1,10 @@
+export type childrenProps = {
+    id: string;
+    bloodType: string;
+    birth: string;
+    grade: number;
+    name: string;
+    parents: string[];
+    photograph: string;
+    registration: string;
+}
