576220741dd2a18c443de39528948ce94e93b4aa
Fellipe97 <lfalmeida97@hotmail.com>
2023-12-03 21:18:56 -0300

    update

--- src/screens/Grades.tsx
+++ src/screens/Grades.tsx
@@ -8,6 +8,9 @@ import { ScrollViewChildren } from "@components/ScrollViewChildren";
 import { Bimester } from "@components/Bimester";
 
 import { MaterialIcons } from '@expo/vector-icons';
+import Api from "../helpers/Api";
+import { useAuth } from "@hooks/auth";
+import { childrenProps } from "../types/children";
 
 
 type propsChildren = {
@@ -29,9 +32,13 @@ type propsGrades = {
 
 export function Grades() {
     const navigation = useNavigation();
+    const api = Api();
+    const { user } = useAuth()
 
 
-    const children: propsChildren[] = [
+
+    const [children, setChildren] = useState<childrenProps[] | null>(null)
+    /* const children: propsChildren[] = [
         {
             id: 'aaaaaaa111111',
             name: 'Maria Laura da Silva Oliveira',
@@ -62,38 +69,155 @@ export function Grades() {
             id_grade: 'ensino_Fundamental_II',
             photograph: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
         },
-    ]
+    ] */
 
     const grades: propsGrades[] = [
         {
-            idChildren: 'aaaaaaa111111',
-            school_subject: 'Portugues',
-            grade: '4,8',
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'Português',
+            grade: '4,6',
             bimester: 1
         },
         {
-            idChildren: 'aaaaaaa111111',
-            school_subject: 'Matematica',
-            grade: '8,8',
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'Português',
+            grade: '6,8',
             bimester: 2
         },
         {
-            idChildren: 'aaaaaaa111111',
-            school_subject: 'Fisica',
-            grade: '9,8',
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'Português',
+            grade: '7,3',
             bimester: 3
         },
         {
-            idChildren: 'aaaaaaa111111',
-            school_subject: 'Educacao Fisica',
-            grade: '9,8',
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'Português',
+            grade: '5,1',
+            bimester: 4
+        },
+        {
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'Matemática',
+            grade: '8,9',
             bimester: 1
         },
         {
-            idChildren: 'bbbbbbb2222222',
-            school_subject: 'Historia',
-            grade: '1,8',
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'Matemática',
+            grade: '9,8',
+            bimester: 2
+        },
+        {
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'Matemática',
+            grade: '2,4',
+            bimester: 3
+        },
+        {
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'Matemática',
+            grade: '8,2',
+            bimester: 4
+        },
+        {
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'História',
+            grade: '9,6',
             bimester: 1
+        },
+        {
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'História',
+            grade: '7,3',
+            bimester: 2
+        },
+        {
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'História',
+            grade: '4,2',
+            bimester: 3
+        },
+        {
+            idChildren: 'TxquPuSseEdBXsZJcYen',
+            school_subject: 'História',
+            grade: '8,6',
+            bimester: 4
+        },
+
+
+
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'Matemática',
+            grade: '7,5',
+            bimester: 1
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'Matemática',
+            grade: '6,3',
+            bimester: 2
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'Matemática',
+            grade: '9,4',
+            bimester: 3
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'Matemática',
+            grade: '3,4',
+            bimester: 4
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'Português',
+            grade: '3,5',
+            bimester: 1
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'Português',
+            grade: '6,2',
+            bimester: 2
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'Português',
+            grade: '9,4',
+            bimester: 3
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'Português',
+            grade: '7,2',
+            bimester: 4
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'História',
+            grade: '8,4',
+            bimester: 1
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'História',
+            grade: '7,2',
+            bimester: 2
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'História',
+            grade: '3,4',
+            bimester: 3
+        },
+        {
+            idChildren: 'Wf3dXr6IHRQb8aZMLJgp',
+            school_subject: 'História',
+            grade: '9,2',
+            bimester: 4
         }
     ]
 
@@ -112,12 +236,31 @@ export function Grades() {
         navigation.goBack();
     }
 
+    const getGeneralInformationChildren = async (tokens: string[]) => {
+        let resp = await api.getGeneralInformationChildren(tokens)
+        if (resp) {
+            setChildren(resp)
+        }
+    }
 
     useEffect(() => {
-        if (children.length > 0 && !childrenSelected) {
+        if (user?.children) {
+            getGeneralInformationChildren(user?.children)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (children && children.length > 0 && !childrenSelected) {
             setChildrenSelected(children[0]);
         }
     }, [children, childrenSelected]);
+
+
+    /* useEffect(() => {
+        if (children.length > 0 && !childrenSelected) {
+            setChildrenSelected(children[0]);
+        }
+    }, [children, childrenSelected]); */
 
 
     useEffect(() => {
