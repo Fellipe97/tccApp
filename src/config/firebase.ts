1c2da79e73c78a3a20fb637a41f9ce4912a0c18c
Fellipe97 <lfalmeida97@hotmail.com>
2024-01-30 15:22:38 -0300

    update

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBtOfDqsAMFMOOZeLmeYNCrvBcG3fQqZog",
-  authDomain: "uerj-tcc.firebaseapp.com",
-  projectId: "uerj-tcc",
-  storageBucket: "uerj-tcc.appspot.com",
-  messagingSenderId: "963690499470",
-  appId: "1:963690499470:web:d41c5b611ab6fedf1e68df",
-  measurementId: "G-NQ754CSPVC"
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXXXX",
+  appId: "XXXX",
+  measurementId: "XXXX"
 };
 
 
